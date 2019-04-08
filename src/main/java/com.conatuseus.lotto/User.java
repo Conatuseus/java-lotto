5bf2fb1a1b589fc6b880318e4b2a8a2f4131a1fe
conatuseus <conatuseus@gmail.com>
2019-04-08 10:43:32 +0900

    Add User class for input money

---
+++ src/main/java/com.conatuseus.lotto/User.java
@@ -0,0 +1,18 @@
+package com.conatuseus.lotto;
+
+import com.sun.tools.javac.util.List;
+
+import java.util.Scanner;
+
+public class User {
+    private static final Scanner sc=new Scanner(System.in);
+    private List<Lotto> lottoList;
+    private int money;
+
+    public int inputMoney(){
+
+
+        return 0;
+    }
+
+}
