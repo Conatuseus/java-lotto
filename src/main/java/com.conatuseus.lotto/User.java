028bd68e78adae439bdb4306f6ef468b2f0f2763
conatuseus <conatuseus@gmail.com>
2019-04-08 11:06:56 +0900

    Add input user's money function

--- src/main/java/com.conatuseus.lotto/User.java
+++ src/main/java/com.conatuseus.lotto/User.java
@@ -1,18 +1,31 @@
 package com.conatuseus.lotto;
 
-import com.sun.tools.javac.util.List;
 
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class User {
-    private static final Scanner sc=new Scanner(System.in);
-    private List<Lotto> lottoList;
+    private static final BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    private List<Lotto> lottoList=new LinkedList<>();
     private int money;
 
-    public int inputMoney(){
+    public int getMoney() {
+        return money;
+    }
 
+    private void setMoney(int money) {
+        this.money = money;
+    }
 
-        return 0;
+    public void inputMoney() throws IOException {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input=br.readLine();
+        this.setMoney(Integer.parseInt(input));
     }
 
 }
