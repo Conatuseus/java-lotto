bbe629942eab6ba2f4ee55556055fdb33563085d
conatuseus <conatuseus@gmail.com>
2019-04-09 11:58:25 +0900

    Add variable for counting rank in Rank

--- src/main/java/com.conatuseus.lotto/model/Rank.java
+++ src/main/java/com.conatuseus.lotto/model/Rank.java
@@ -4,21 +4,30 @@ package com.conatuseus.lotto.model;
  * 로또 등수를 의미하는 enum
  */
 public enum Rank {
-    FIRST(6, 2_000_000_000), // 1등
-    SECOND(5, 30_000_000), // 2등
-    THIRD(5, 1_500_000), // 3등
-    FOURTH(4, 50_000), // 4등
-    FIFTH(3, 5_000), // 5등
-    MISS(0, 0);
+    FIRST(6, 2_000_000_000,0), // 1등
+    SECOND(5, 30_000_000,0), // 2등
+    THIRD(5, 1_500_000,0), // 3등
+    FOURTH(4, 50_000,0), // 4등
+    FIFTH(3, 5_000,0), // 5등
+    MISS(0, 0,0);
 
     private static final int WINNING_MIN_COUNT = 3;
 
     private int countOfMatch;
     private int winningMoney;
+    private int numberOfThisRank;
 
-    private Rank(int countOfMatch, int winningMoney) {
+    private Rank(int countOfMatch, int winningMoney, int numberOfThisRank) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
+        this.numberOfThisRank=numberOfThisRank;
+    }
+    private void setNumberOfThisRank(int numberOfThisRank){
+        this.numberOfThisRank=numberOfThisRank;
+    }
+
+    public int getNumberOfThisRank(){
+        return numberOfThisRank;
     }
 
     public int getCountOfMatch() {
@@ -49,6 +58,14 @@ public enum Rank {
 
     private boolean matchCount(int countOfMatch) {
         return this.countOfMatch == countOfMatch;
+    }
+
+    private void increaseNumberOfThisRank(){
+        this.setNumberOfThisRank(this.getNumberOfThisRank()+1);
+    }
+
+    public long moneyCalculator(){
+        return this.getWinningMoney() * this.getNumberOfThisRank();
     }
 }
 
